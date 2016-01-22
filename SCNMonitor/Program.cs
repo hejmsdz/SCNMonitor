a8791aafc175b513070bc838ffb98de79db10828
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2016-01-22 15:49:52 +0000

    Allow one instance at a time

--- SCNMonitor/Program.cs
+++ SCNMonitor/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,31 @@ namespace SCNMonitor
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(true, "{21ef2031-be0a-4196-bfc2-3162930ff0a2}");
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                try
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainWindow());
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
+            else
+            {
+                MessageBox.Show("The application is already running - you can find it in the system tray.");
+            }
         }
     }
 }
