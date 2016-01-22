22bc798ea541de0bf7ed057ab5c34afc41f59628
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2016-01-22 11:52:26 +0000

    Reload the data every minute

--- SCNMonitor/MainWindow.cs
+++ SCNMonitor/MainWindow.cs
@@ -12,15 +12,41 @@ namespace SCNMonitor
 {
     public partial class MainWindow : Form
     {
+        const int RELOAD_INTERVAL = 60;
+
         private SCNClient scn;
+        private Timer timer;
+        private int timeToReload;
+
         public MainWindow()
         {
             InitializeComponent();
             scn = new SCNClient("http://www.scn.put.poznan.pl/main.php");
+            timer = new Timer();
+            timer.Tick += new EventHandler(OnTimer);
+
+            timer.Interval = 1000;
+            timer.Start();
+            timeToReload = 0;
+        }
+
+        private async void OnTimer(object sender, EventArgs e)
+        {
+            timeToReload--;
+            if (timeToReload <= 0)
+            {
+                await CheckTransfer();
+            }
         }
 
         private async Task CheckTransfer()
         {
+            timer.Stop();
+
+            string buttonText = check.Text;
+            check.Text = "Checking...";
+            check.Enabled = false;
+
             try
             {
                 await scn.Transfer();
@@ -29,6 +55,13 @@ namespace SCNMonitor
             {
                 MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
+            }
+            finally
+            {
+                check.Text = buttonText;
+                check.Enabled = true;
+                timeToReload = RELOAD_INTERVAL;
+                timer.Start();
             }
 
             download.Text = scn.Download.ToString() + " GB";
@@ -40,12 +73,7 @@ namespace SCNMonitor
 
         private async void login_Click(object sender, EventArgs e)
         {
-            string text = check.Text;
-            check.Text = "Checking...";
-            check.Enabled = false;
             await CheckTransfer();
-            check.Text = text;
-            check.Enabled = true;    
         }
     }
 }
